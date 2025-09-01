19643e84946a8cd4620321a78de5c7542fa19056
David Witt <midknight55@gmail.com>
2025-08-31 20:37:25 -0700

    Configured redirect logic

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,10 +1,24 @@
+import { getDestinationForCountry } from '@/helpers/route-ops';
 import { getLink } from '@repo/data-ops/queries/links';
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
 import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
 	const { id } = c.req.param();
-	const linkDatafromDb = await getLink(id);
-	return c.json({ linkDatafromDb });
+	const linkInfo = await getLink(id);
+
+	if (!linkInfo) {
+		return c.text('Destination not found', 404);
+	}
+
+	const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+	if (!cfHeader.success) {
+		return c.text('Invalid Cloudflare headers', 400);
+	}
+
+	const headers = cfHeader.data;
+	const destination = getDestinationForCountry(linkInfo, headers.country);
+	return c.redirect(destination);
 });
